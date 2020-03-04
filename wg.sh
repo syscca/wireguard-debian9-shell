55b50a95f277b3c285b3453840aa73998c7575c8
寻味漂漂 <vinyo@users.noreply.github.com>
2020-03-04 12:29:37 +0800

    Update wg.sh

--- wg.sh
+++ wg.sh
@@ -4,7 +4,7 @@ echo "deb http://deb.debian.org/debian/ unstable main" > /etc/apt/sources.list.d
 printf 'Package: *\nPin: release a=unstable\nPin-Priority: 90\n' > /etc/apt/preferences.d/limit-unstable
 apt update
 # 安全更新
-env DEBIAN_FRONTEND=noninteractive APT_LISTCHANGES_FRONTEND=mail apt-get upgrade -y -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold'
+env DEBIAN_FRONTEND=noninteractive apt-get upgrade -y -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold'
 #　安装wireguard
 apt install -y wireguard qrencode curl
 # 开启BBR
